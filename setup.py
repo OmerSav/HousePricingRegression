bd70c756df1db2a2146a0b89b17aec289fb26125
Ömer Savranoğulları <omersavran13@gmail.com>
2022-11-09 12:12:26 +0300

    9.11.2022

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project task from "[Kaggle House Prices - Advanced Regression Techniques](https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques)". ',
+    author='Ömer Savranoğulları',
+    license='',
+)
